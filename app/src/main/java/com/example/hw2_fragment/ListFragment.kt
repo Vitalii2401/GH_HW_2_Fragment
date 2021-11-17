2beef50c34eb5688c40726353a266fe588681cc5
Vitalii <movsesyan.vitaliy24ua@gmail.com>
2021-11-17 15:10:09 +0200

    Add string array for name car. App name changed

--- app/src/main/java/com/example/hw2_fragment/ListFragment.kt
+++ app/src/main/java/com/example/hw2_fragment/ListFragment.kt
@@ -5,8 +5,15 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.LifecycleOwner
+import com.example.hw2_fragment.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+    lateinit var binding: FragmentListBinding
+    private val dataModel: DataModel by activityViewModels()
+    var name = arrayOf("")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,6 +21,10 @@ class ListFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_list, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
     }
 
     companion object {
