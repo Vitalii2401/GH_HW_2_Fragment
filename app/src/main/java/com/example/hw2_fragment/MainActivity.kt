baeaeee865b1207fdd29b88726a9197540c26507
Vitalii <movsesyan.vitaliy24ua@gmail.com>
2021-11-17 12:36:40 +0200

    Add fun for open fragments

--- app/src/main/java/com/example/hw2_fragment/MainActivity.kt
+++ app/src/main/java/com/example/hw2_fragment/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.hw2_fragment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.hw2_fragment.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var  binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        openFrag(ListFragment.newInstance(), R.id.frameForList)
+        openFrag(SelectFragment.newInstance(), R.id.frameForSelect)
+    }
+
+    private fun openFrag(f: Fragment, idHolder: Int){
+        supportFragmentManager
+            .beginTransaction()
+            .replace(idHolder, f)
+            .commit()
     }
 }
