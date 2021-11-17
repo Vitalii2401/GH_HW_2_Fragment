210899b47ab47f5e77185c8c8af938dd8faaaa4a
Vitalii <movsesyan.vitaliy24ua@gmail.com>
2021-11-17 15:04:44 +0200

    Send message on click button

--- app/src/main/java/com/example/hw2_fragment/DataModel.kt
+++ app/src/main/java/com/example/hw2_fragment/DataModel.kt
@@ -1,6 +1,10 @@
 package com.example.hw2_fragment
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class DataModel : ViewModel() {
+    val message: MutableLiveData<String> by lazy {
+        MutableLiveData<String>()
+    }
 }
--- app/src/main/java/com/example/hw2_fragment/SelectFragment.kt
+++ app/src/main/java/com/example/hw2_fragment/SelectFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import com.example.hw2_fragment.databinding.FragmentSelectBinding
 
 class SelectFragment : Fragment() {
+    lateinit var binding: FragmentSelectBinding
+    private val dataModel: DataModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -14,6 +18,24 @@ class SelectFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_select, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.btnGermany.setOnClickListener {
+            dataModel.message.value = "Germany"
+        }
+
+        binding.btnItaly.setOnClickListener {
+            dataModel.message.value = "Italy"
+        }
+
+        binding.btnFrance.setOnClickListener {
+            dataModel.message.value = "France"
+        }
+
+        binding.btnAmerica.setOnClickListener {
+            dataModel.message.value = "America"
+        }
     }
 
     companion object {
